2414e46c1d5cfee788be9a877398bee1347ed592
Lethícia Marcelino dos Santos Moreira <marlom1012@gmail.com>
2021-09-04 15:59:00 -0400

    optimize log

--- startup/logger.js
+++ startup/logger.js
@@ -11,12 +11,21 @@ process
 // winston.exceptions.handle(new winston.transports.File({ filename: 'exceptions.log' }))
 
 const logger = winston.createLogger({
-  format: winston.format.combine(winston.format.errors({ stack: true }), winston.format.json()),
+  format: winston.format.combine(
+    winston.format.errors({ stack: true }),
+    winston.format.json(),
+    winston.format.colorize(),
+    winston.format.prettyPrint(),
+    winston.format.timestamp(),
+    winston.format.align()
+    
+  ),
   transports: [
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'info.log', level: 'info' })
+    new winston.transports.File({ filename: 'info.log', level: 'info' }),
+    new winston.transports.Console()
   ],
-  exceptionHandlers: [new winston.transports.File({ filename: 'exceptions.log' })],
+  exceptionHandlers: [new winston.transports.File({ filename: 'exceptions.log' }), new winston.transports.Console()],
 
   exitOnError: false
 })
